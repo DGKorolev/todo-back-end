46e0530add4ec22fdbec104457405faa65e560a8
Dmitry Korolev <korolev@sfedy.ru>
2021-09-07 17:46:30 +0300

    change build script in package.json

--- config/config.js
+++ config/config.js
@@ -6,13 +6,6 @@ module.exports = {
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
-  },
   "production": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
