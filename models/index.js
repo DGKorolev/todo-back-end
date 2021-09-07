eb4524c381c9c9a83798c7cd27c3f508a176a559
Dmitry Korolev <korolev@sfedy.ru>
2021-09-07 18:45:16 +0300

    change models/index.js

--- models/index.js
+++ models/index.js
@@ -9,9 +9,10 @@ const config = require(__dirname + '/../config/config')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
+
+if (env === 'production'){
+  sequelize = new Sequelize(process.env.DATABASE_URL);
+}else{
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
