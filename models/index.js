b99b414b7b2f0b9bd48c9f9434c1ef1e1428e2a0
Dmitry Korolev <korolev@sfedy.ru>
2021-09-07 21:55:56 +0300

    change models/index.js

--- models/index.js
+++ models/index.js
@@ -13,7 +13,13 @@ let sequelize;
 console.log('this is ' + process.env.NODE_ENV + ' mode')
 
 if (env === 'production'){
-  sequelize = new Sequelize(process.env.DATABASE_URL);
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: true
+    }
+  });
 }else{
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
