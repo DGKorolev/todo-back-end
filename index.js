7912457e8b244b0a9b1f2b0d2c1b6f8623255130
DGKorolev <dg.korolev.here@gmail.com>
2021-09-10 14:18:41 +0300

    change index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3001',
+    origin: process.env.API_URL,
     credentials: true,
     exposedHeaders: ["set-cookie"],
 }));
