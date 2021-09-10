d80fd8554b84b474bd4fc007f6d8fb1c6cade1ea
DGKorolev <dg.korolev.here@gmail.com>
2021-09-10 17:42:58 +0300

    change config.js

--- config/config.js
+++ config/config.js
@@ -3,24 +3,7 @@ module.exports = {
     "username": "root_user",
     "password": "root_user",
     "database": "todo_db",
-    "host": "127.0.0.1",
+    "host": "127.0.0.5",
     "dialect": "postgres"
   },
-  "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": "postgres",
-    "port": process.env.DB_PORT,
-    "dialectOptions": {
-      "ssl": true
-    },
-    "pool": {
-      "max": 5,
-      "min": 0,
-      "acquire": 30000,
-      "idle": 10000
-    }
-  }
 }
